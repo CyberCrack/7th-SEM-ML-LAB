e25dd34a4bff21bf31e53756735200c2b64b6d95
Cyber Crack <mayankanuragi2013@gmail.com>
2020-12-16 23:19:21 +0530

    Adding Bayesian Network.py

---
+++ Bayesian Network.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from pgmpy.estimators import MaximumLikelihoodEstimator
+from pgmpy.inference import VariableElimination
+from pgmpy.models import BayesianModel
+
+dataset = pd.read_csv('Heart Disease.csv')
+
+model = BayesianModel([('age', 'trestbps'), ('age', 'fbs'), ('sex', 'trestbps'), ('sex', 'trestbps'),
+					   ('exang', 'trestbps'), ('trestbps', 'heartdisease'), ('fbs', 'heartdisease'),
+					   ('heartdisease', 'restecg'), ('heartdisease', 'thalach'), ('heartdisease', 'chol')])
+
+print('\nLearning CPDs using Maximum Likelihood Estimators...')
+model.fit(dataset, estimator=MaximumLikelihoodEstimator)
+
+print('\nInferencing with Bayesian Network:')
+HeartDisease_infer = VariableElimination(model)
+
+q = HeartDisease_infer.query(variables=['heartdisease'], evidence={'age': 37, 'sex': 1})
+print(q)
+
+q = HeartDisease_infer.query(variables=['heartdisease'], evidence={'age': 37})
+print(q)
+
+q = HeartDisease_infer.query(variables=['heartdisease'], evidence={'sex': 1})
+print(q)
